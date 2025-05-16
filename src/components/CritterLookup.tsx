7e4fd9796d4c04dc4562d5dfbd927bd16e271e9e
Leslie Murphy <16138931+dev-byte-leslie@users.noreply.github.com>
2025-05-15 19:53:58 -0500

    more lint updates

--- src/components/CritterLookup.tsx
+++ src/components/CritterLookup.tsx
@@ -1,15 +1,19 @@
-import { loadFishList } from '../services/FishList.tsx';
+import { loadFishList } from '../services/FishList.ts';
 import {Button} from "@mui/material";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import { Fish } from '../types/Fish';
 
 const CritterLookup = () => {
     const [fishList, setFishList] = useState<Fish[]>([]);
 
+    useEffect(() => {
+        console.log(fishList);
+    }, [fishList]);
+
     const handleButtonClick = async () => {
         const fishData = await loadFishList();
         setFishList(fishData);
-    };
+    }
 
     return (
         <Button variant="contained" color="primary" onClick={handleButtonClick}>
